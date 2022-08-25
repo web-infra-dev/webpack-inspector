9be444d2180aafdffef4f5d0e22572f94c3e9b7f
yangxingyuan <yangxingyuan@bytedance.com>
2022-08-25 20:30:51 +0800

    chore: delete useless code

--- packages/webpack-plugin/src/utils.ts
+++ packages/webpack-plugin/src/utils.ts
@@ -161,7 +161,6 @@ export function readDirectory(
         ),
       );
     } else {
-      debugger;
       directory.children.push({
         path: relativeItemPath,
         async: fileToIsAsyncChunkMap.get(relativeItemPath),
